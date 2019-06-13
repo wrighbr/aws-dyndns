5ef8d3b44d67969adf71258b549888a1f96caa0a
Brett Wright <wright.brett@gmail.com>
2019-06-13 18:41:47 +0200

    added extra logging and fmting

--- aws-dyndns.py
+++ aws-dyndns.py
@@ -1,53 +1,72 @@
-import socket, re, boto3, urllib3, argparse
+import socket
+import re
+import boto3
+import urllib3
+import argparse
+import time
+from datetime import datetime
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dns')
 parser.add_argument('--id')
 args = parser.parse_args()
 
-dnsname =  args.dns 
-zoneid = args.id 
+
+dnsname = args.dns
+zoneid = args.id
 
 
-print('DNS recored: ' + dnsname)
-print('ZONE ID: ' + zoneid)
- 
+now = datetime.now()
+
 url = 'http://ipecho.net/plain'
 
 
 http = urllib3.PoolManager()
 r = http.request('GET', url)
 
-#old_ip = socket.gethostbyname(dnsname)
 new_ip = re.sub("(b|')", "", str(r.data))
-
 
 client = boto3.client('route53')
 
-print('INFO: Changing dns recorded ' + dnsname + ' to ' + new_ip)
-
-
-client.change_resource_record_sets(
-HostedZoneId = zoneid,
-
-ChangeBatch = {
-    "Comment": "Update record to reflect new IP Public address",
-    "Changes": [
-        {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": dnsname,
-                "Type": "A",
-                "TTL": 60,
-                "ResourceRecords": [
-                    {
-                        "Value": new_ip
-                    }
-                ]
-            }
-        }
-    ]
-}
-
+zone = client.list_resource_record_sets(
+    HostedZoneId=zoneid,
+    StartRecordName=dnsname
 )
 
+rrs = zone['ResourceRecordSets']
+current_ip = rrs[0]['ResourceRecords'][0]['Value']
+
+
+print(str(now) + ': DNS recored: ' + str(dnsname))
+print(str(now) + ': ZONE ID: ' + str(zoneid))
+print(str(now) + ': Current IP Address is ' + str(current_ip))
+
+print(str(now) + ': Checking if IP address has changed')
+
+if current_ip != new_ip:
+    print(str(now) + ': IP address has changed. Now Changing dns recorded ' +
+        str(dnsname) + ' to ' + str(new_ip))
+
+    client.change_resource_record_sets(
+                HostedZoneId=zoneid,
+                ChangeBatch={
+                    "Comment": "Update record to reflect new IP Public address",
+                    "Changes": [
+                        {
+                            "Action": "UPSERT",
+                            "ResourceRecordSet": {
+                                "Name": dnsname,
+                                "Type": "A",
+                                "TTL": 60,
+                                "ResourceRecords": [
+                                    {
+                                        "Value": new_ip
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            )
+        
